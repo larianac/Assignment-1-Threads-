d1dde24560d639bbc57307a828dea0b31d9a202f
larianac <91437764+larianac@users.noreply.github.com>
2023-10-09 22:57:26 -0700

    Update thread.c

--- thread.c
+++ thread.c
@@ -1,3 +1,6 @@
+/*NOTES
+
+
 #include <stdio.h>
 
 int var = 0;
@@ -24,4 +27,49 @@ pthread_join(t1, NULL);
 pthread_join(t2, NULL);
 printf(“var = %d\n”, var);
 }
+
+*/
+
+//CODE
+
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <unistd.h> 
+#include <pthread.h> 
+
+int g = 0; 
+
+void *Threads(void *var) 
+{ 
+    // Store the value argument passed to this thread 
+    int *x = (int *)var; 
+
+    // Sum of array
+    int sum = 0;
+  
+    // static variable to observe its changes 
+    static int y = 0; 
+    
+    
+    // checks if it is addition or subtraction
+  
+    //loop through array
+    /*
+    access variables and add together 
+    */
+    //loop through array again and add or subtract sum  
+    //print results
+} 
+  
+int main() 
+{ 
+    int i; 
+    pthread_t x; 
+  
+    // Let us create three threads 
+    for (i = 0; i < 3; i++) 
+        pthread_create(&x, NULL, Threads, (void *)&x); 
+   pthread_exit(NULL); 
+    return 0; 
+} 
 
